c01e827b3ba53227487d50116116a1c69c3f37a2
Vlad Ĉereŝna <vladceresna6@gmail.com>
2024-09-22 19:55:22 +0200

    Fixes

--- composeApp/src/commonMain/kotlin/com/vladceresna/virtel/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.vladceresna.virtel
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/vladceresna/virtel/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.vladceresna.virtel
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
