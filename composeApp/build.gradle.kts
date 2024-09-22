d9b976e1c8e4e179ef9832e01483ea08cd4db567
Vlad Ĉereŝna <vladceresna6@gmail.com>
2024-09-22 17:23:43 +0200

    Startup Setup

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -40,12 +40,13 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
--- composeApp/src/commonMain/kotlin/com/vladceresna/virtel/App.kt
+++ composeApp/src/commonMain/kotlin/com/vladceresna/virtel/App.kt
@@ -1,37 +1,57 @@
 package com.vladceresna.virtel
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.datetime.*
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import virtel.composeapp.generated.resources.Res
-import virtel.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        val greeting = remember { Greeting().greet() }
+        Column (
+            Modifier.fillMaxSize(),
+            Arrangement.Center,
+            Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Virtel",
+                modifier = Modifier.padding(10.dp),
+                fontWeight = FontWeight(900),
+                fontSize = 75.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "made by\nVlad Ceresna",
+                modifier = Modifier.padding(20.dp),
+                fontWeight = FontWeight(500),
+                color = Color(0,200,0,255),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
         }
     }
+}
+
+fun todaysDate(): String {
+    fun LocalDateTime.format() = toString().substringBefore('T')
+
+    val now = Clock.System.now()
+    val zone = TimeZone.currentSystemDefault()
+    return now.toLocalDateTime(zone).format()
 }
