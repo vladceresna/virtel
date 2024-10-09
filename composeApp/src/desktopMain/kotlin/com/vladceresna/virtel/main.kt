e19ce2388430c4b3c920f7c273d900b6a73b084d
Vlad Ĉereŝna <vladceresna6@gmail.com>
2024-10-09 23:52:17 +0300

    Some brand changes

--- composeApp/src/desktopMain/kotlin/com/vladceresna/virtel/main.kt
+++ composeApp/src/desktopMain/kotlin/com/vladceresna/virtel/main.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.window.application
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "virtel",
+        title = "Virtel",
     ) {
         App()
     }
