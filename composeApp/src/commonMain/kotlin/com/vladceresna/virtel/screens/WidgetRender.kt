c4e0b9af462064494cf2897bfb8071240b23866c
Vlad Ĉereŝna <vladceresna6@gmail.com>
2024-10-15 09:54:19 +0300

    some changes

--- composeApp/src/commonMain/kotlin/com/vladceresna/virtel/screens/WidgetRender.kt
+++ composeApp/src/commonMain/kotlin/com/vladceresna/virtel/screens/WidgetRender.kt
@@ -105,6 +105,8 @@ fun Widget(rd:Boolean, model: WidgetModel, modifier: Modifier){
                 onValueChange = {
                     model.value = it
                     value = it
+                    model.value = value
+                    value = model.value
                     DataStore.put(model.appId,DataType.VIEW,model.name,model)
                 }
             )
@@ -114,6 +116,8 @@ fun Widget(rd:Boolean, model: WidgetModel, modifier: Modifier){
                         delay(10)
                         if (model.value != value){
                             value = model.value
+                            model.value = value
+                            DataStore.put(model.appId,DataType.VIEW,model.name,model)
                         }
                     }
                 }
