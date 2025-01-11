603dfacf87e3b6531d1d3580518a38b46978762c
Vlad Ĉereŝna <vladceresna6@gmail.com>
2025-01-11 12:22:08 +0200

    DTM Date And Time Now and Format

--- composeApp/src/commonMain/kotlin/com/vladceresna/virtel/screens/LoadingScreen.kt
+++ composeApp/src/commonMain/kotlin/com/vladceresna/virtel/screens/LoadingScreen.kt
@@ -1,9 +1,12 @@
 package com.vladceresna.virtel.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,7 +15,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.vladceresna.virtel.controllers.VirtelSystem
+import coil3.compose.AsyncImage
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun LoadingScreen(){
@@ -21,6 +25,7 @@ fun LoadingScreen(){
         Arrangement.Center,
         Alignment.CenterHorizontally
     ) {
+
         Text(
             text = "Virtel",
             fontSize = 100.sp,
@@ -32,6 +37,12 @@ fun LoadingScreen(){
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
             color = Color.hsv(135f, 0.8f,0.3f)
+        )
+        Text(
+            text = "If loading is too long, try to check your\n internet connection and restart the app",
+            fontSize = 10.sp,
+            fontWeight = FontWeight.Medium,
+            color = MaterialTheme.colorScheme.onBackground
         )
     }
 }
