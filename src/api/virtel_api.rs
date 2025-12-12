9f185d1781a9351ea01494f297774c3d9671fd4d
Vlad Ĉereŝna <vladceresna6@gmail.com>
2025-12-12 23:59:34 +0200

    Android adding and Result class

--- src/api/virtel_api.rs
+++ src/api/virtel_api.rs
@@ -82,6 +82,56 @@ impl Permissions {
 
 pub fn virtel_api_wren_bindings() -> &'static str {
     return r#"
+class Result {
+	construct ok(data) {
+		_data = data
+		_status = true
+	}
+	construct error(msg) {
+		_msg = msg
+		_status = false
+	}
+	status { _status }
+	msg { _msg }
+	data { _data }
+	unwrap() {
+		if (_status) {
+			return _data
+		} else {
+			Log.error(_msg)
+			Fiber.abort(_msg)
+		}
+	}
+	unwrapOr(default) {
+		if (_status) {
+			return _data
+		} else {
+			Log.warning(_msg)
+			return default
+		}
+	}
+	unwrapOrElse(closure) {
+        if (_status) {
+            return _data
+        } else {
+            return closure.call(_msg)
+        }
+    }
+    then(closure) {
+        if (_status) {
+    		return closure.call(_data)
+        } else {
+		    return this
+        }
+	}
+	consume(onSuccess, onError) {
+		if (_status) {
+            onSuccess.call(_data)
+		} else {
+            onError.call(_msg)
+		}
+	}
+}
 class Log {
     foreign static info(msg)
     foreign static success(msg)
